ee8d1ae47faef5e520e8463fee027b3deef1fb33
Sviatozar Matiash <sviatozar123@gmail.com>
2024-09-30 18:18:09 +0300

    finalization

--- first_hw.py
+++ first_hw.py
@@ -3,9 +3,10 @@ from datetime import datetime
 def get_days_from_today(date):
     today = datetime.today()
 
-    date = datetime.strptime(date, "%Y-%m-%d")
+    try:
+        date = datetime.strptime(date, "%Y-%m-%d")
+    except:
+        print("Incorrect input format!")
 
     result = (today - date).days
     return result
-
-print(get_days_from_today("2024-6-28"))
--- second_hw.py
+++ second_hw.py
@@ -1,19 +1,16 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    if min < 1 or max > 1000:
-        raise Exception("Min value supposed to be >= 1 and max value <= 1000!")
-    if quantity < min or quantity > max:
-        raise Exception("Quantity supposed to be between min and max!")
     
     result = []
+
+    if min < 1 or max > 1000:
+        return result
+    
+    if quantity <= 0:
+        return result
 
     for i in range(quantity):
         result.append(random.randint(min, max))
 
-    return list(sorted(set(result)))
-        
-
-
-lottery_numbers = get_numbers_ticket(1, 49, 6)
-print("Ваші лотерейні числа:", lottery_numbers)
+    return sorted(set(result))
